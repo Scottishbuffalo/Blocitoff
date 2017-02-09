448da9952d743474b3a40e17eeba86732088479b
Mike MacAdam <mikemacadam87@gmail.com>
2017-02-08 16:59:40 -0800

    checkpoint 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
-   def show
-     @user = current_user
-     @new_item = Item.new
-   end
- end
+  before_action :check_user
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def check_user
+    @user = User.find(params[:id])
+    if @user != current_user
+      flash[:alert] = "Sorry, you are not allow to see other user's page."
+      redirect_to root_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+
   resources :items
 
-  get 'users/show'
+  resources :users, only: [:show]
 
   authenticated :user do
     root 'items#index', as: :authenticated_root
@@ -10,6 +12,5 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  devise_for :users, controllers: { sessions: 'users/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
